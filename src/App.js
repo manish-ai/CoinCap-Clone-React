ce536412b3d51b8b7d6d7ad7f4f3be4d69ac1f81
Manish N <manish-ai>
2021-07-19 21:54:47 +0530

    movie-cards

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import HomePage from "./Components/HomePage/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
